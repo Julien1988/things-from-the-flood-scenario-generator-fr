b803be78bf0818df5fb564c4e17f6b42430f0107
Julien Broens <julien-broens@MBP-de-Julien.lan>
2020-08-07 11:13:59 +0200

    add antagonist & behaviour

--- index.js
+++ index.js
@@ -1,1 +1,136 @@
-console.log('coucou')
+const antagonist = {
+    Humain: [
+        'Savant névrosé',
+        'Serviteur captif',
+        'Philanthrope fou',
+        'Stratège impitoyable',
+        'Victime psychotique',
+        'Artiste passionné'
+
+    ],
+    Animal: [
+        'Insectes',
+        'Prédateur',
+        'Herbivores craintifs',
+        'Reptiles égarés',
+        'Groupe de petits nuisibles',
+        'Oiseaux sauvages'
+    ],
+    Végétal: [
+        'Plantes empoisonnées',
+        'Plantes intelligentes',
+        'Plantes carnivores',
+        'Plantes à croissance accélérée',
+        'Lianes étrangleuses',
+        'Spores ou pollen'
+    ],
+    Machine: [
+        'Turbine flottante',
+        'Poteau ou antenne',
+        'Machine souterraine',
+        'Plateforme flottante',
+        'Sphère d’écho ou cube',
+        'Anneaux de vibration'
+    ],
+    Intelligence_artificielle: [
+        'Cyborg',
+        'Virus informatique',
+        'Nano robots',
+        'Robot autonome',
+        'Super ordinateur',
+        'Esprit ruchet'
+    ],
+    Créature: [
+        'Animal préhistorique',
+        'Mort-vivant',
+        'Monstre mythique',
+        'Technologie bio-infectée',
+        'Entité paranormale',
+        'Extraterrestres'
+    ]
+}
+
+const behaviour = {
+    Motivation: [
+        'Célébrité',
+        'Vaincre un ennemi',
+        'Former une nouvelle famille',
+        'Être vénéré',
+        'Être reconnu',
+        'Rage aveugle',
+        'Trouver l’âme sœur',
+        'Rendre le monde meilleur',
+        'Bien-être',
+        'Vengeance',
+        'Trouver un ami',
+        'Rentrer chez soi',
+        'Aider quelqu’un dans le besoin',
+        'Entend des voix',
+        'Menacé ou victime de chantage',
+        'Collecte d’informations',
+        'Se sentir indispensable',
+        'Avoir le contrôle',
+        'Guidé par une entité supérieure',
+        'Démentir les propos de ses adversaires',
+        'Curiosité',
+        'Amour non réciproque',
+        'Maladie incurable',
+        'Dépassé par quelque chose',
+        'Renaissance',
+        'Perfectionnisme',
+        'Prophétie',
+        'Provoquer une apocalypse',
+        'Bâtir une maison',
+        'Créer',
+        'Protéger un être cher',
+        'Se fondre dans la masse',
+        'Trouver des réponses',
+        'Vivre le grand frisson',
+        'Dominer le monde',
+        'Prouver sa valeur'
+
+
+    ],
+    Instinct: [
+        'Sanguinaire',
+        'Chagrin insondable',
+        'Retrouver son troupeau',
+        'S’échapper',
+        'Éliminer une menace',
+        'Reproduction',
+        'Esprit embrouillé',
+        'Se faire comprendre',
+        'Rester caché',
+        'Rester en bonne santé',
+        'Trouver des alliés',
+        'Le frisson de la chasse',
+        'Se débrouiller seul',
+        'Imiter quelqu’un / quelque chose',
+        'Corrompu ou infecté',
+        'Éduquer sa descendance',
+        'Être à la tête du troupeau',
+        'Joueur',
+        'Au seuil de la mort',
+        'Survie',
+        'Parasite',
+        'Peur',
+        'Trouver de la nourriture',
+        'Retrouver des forces',
+        'Autodéfense',
+        'Automutilation',
+        'Fabriquer un nid',
+        'Protéger le groupe',
+        'Protéger le territoire',
+        'Trouver de l’aide',
+        'Conquérir',
+        'Calmer sa faim',
+        'Le statut',
+        'Symbiose',
+        'Transformation',
+        'Éradication'
+
+    ]
+}
+
+
+console.log(behaviour)
